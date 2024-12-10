9bd34f5d49543b217dc877964d9fea691bc8e374
lipesc <felipedrhouse@hotmail.com>
2024-12-10 16:20:55 -0300

    usuarioRepository feito

--- src/main/java/lipe/com/springsecurity/repository/TarefaRepository.java
+++ src/main/java/lipe/com/springsecurity/repository/TarefaRepository.java
@@ -1,9 +1,15 @@
 package lipe.com.springsecurity.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import lipe.com.springsecurity.model.Tarefa;
+import lipe.com.springsecurity.model.Usuario;
+import lipe.com.springsecurity.model.Tarefa.StatusTarefa;
 
+@Repository
 public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
   List<Tarefa> findByStatus(StatusTarefa status);
 
---
+++ src/main/java/lipe/com/springsecurity/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package lipe.com.springsecurity.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import lipe.com.springsecurity.model.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+  Optional<Usuario> findByUsername(String username);
+}
